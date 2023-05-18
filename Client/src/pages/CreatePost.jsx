d11fec58c00e2bceb261a6ad1b7696e95b918695
Darkcoder-paditya <77530131+Darkcoder-paditya@users.noreply.github.com>
2023-05-18 22:06:12 +0530

    fourth commit

--- Client/src/pages/CreatePost.jsx
+++ Client/src/pages/CreatePost.jsx
@@ -41,7 +41,7 @@ const CreatePost = () => {
         //   console.error(error); // Log any error that occurs
         // });
 // https://a-image-creator.onrender.com
-        const response = await fetch('http://a-image-creator.onrender.com/api/v1/dalle', {
+        const response = await fetch('https://a-image-creator.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -69,7 +69,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://a-image-creator.onrender.com/api/v1/post', {
+        const response = await fetch('https://a-image-creator.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
